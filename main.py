9c97f8b20e6d22ba59c64f092357675558f8f28d
J-hta-n <103252521+J-hta-n@users.noreply.github.com>
2023-05-28 16:29:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
-import os
+# import os
 import telebot
+from datetime import datetime, timedelta
+from FlightSearcher import FlightSearcher
 
-from InfoManager import FlightSearch
+flightSearcher = FlightSearcher()
 
-# BOT_TOKEN = os.environ.get('BOT_TOKEN') Not working for some reason
+# BOT_TOKEN = os.environ.get('BOT_TOKEN')
 BOT_TOKEN = "YOUR TOKEN"
 bot = telebot.TeleBot(BOT_TOKEN)
 
+# Origin City
+ORIGIN_CITY = "SIN"
+
+fromTime = datetime.now()
+toTime = fromTime + timedelta(days=7)
+# testData = flightSearcher.check_flights(ORIGIN_CITY, "LON", fromTime, toTime)
+
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    prompt = bot.send_message(message.chat.id, "Howdy, where would you like to travel to?")
+    bot.register_next_step_handler(prompt, search_flight)  # Next message will call the function
+def search_flight(message):
+    destCountry = message.text
+    result = flightSearcher.check_flights(ORIGIN_CITY, destCountry, fromTime, toTime)
+    reply = f"Here are the results: " \
+              f"Price: ${result.price}\n" \
+              f"Departure date: {result.out_date}\n" \
+              f"Return date: {result.return_date}"
+    bot.reply_to(message, reply)
 
-@bot.message_handler(commands=['flight'])
-def origin_handler(message):
-    text = "Origin:"
-    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(sent_msg, destination_handler)
-
-def destination_handler(message):
-    origin = message.text.capitalize()
-    text = "Destination:"
-    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(sent_msg, date_from_handler, origin)
-
-def date_from_handler(message, origin):
-    destination = message.text.capitalize()
-    text = "Date from:"
-    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(sent_msg, date_to_handler, origin, destination)
-
-def date_to_handler(message, origin, destination):
-    date_from = message.text
-    text = "Date to:"
-    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(sent_msg, date_to_handler, origin, destination, date_from)
-
-def fetch_flight(message, origin, destination, date_from):
-    date_to = message.text
-    flight = FlightSearch.get_flight_data(origin, destination, date_from, date_to)
-    flight_message = f'*Flight: {flight.price}*'
-    bot.send_message(message.chat.id, flight_message, parse_mode="Markdown")
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
 bot.polling()
+
+
+
+
+
+
+

d7efe282d0c4a57a4c60f15506118ea1b6b3d845
J-hta-n <103252521+J-hta-n@users.noreply.github.com>
2023-05-28 16:28:58 +0800

    Update InfoManager.py

--- InfoManager.py
+++ InfoManager.py
@@ -1,30 +1,24 @@
 import requests
-from flight_data import FlightData
+from FlightData import FlightData
 
-TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = "vmickVy9y-5TmvG0yHIvuM-o-PvLMxbH"
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com"
+TEQUILA_API_KEY = "YOUR TOKEN"
 
-class FlightSearch:
-    # This class is responsible for talking to the Flight Search API.
-    # Tequila Flight Search API Docs: https://tequila.kiwi.com/portal/docs/tequila_api
 
-    def get_IATA_code(self, city_name):
-        # Uses Tequila Location API Get to retrieve IATA Code of a city
-        # https://tequila.kiwi.com/portal/docs/tequila_api/locations_api
+class FlightSearcher:
+
+    def get_destination_code(self, city_name):
         location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
-        query = {
-            "term": city_name,
-            "location_types": "city"
-        }
         headers = {"apikey": TEQUILA_API_KEY}
-        response = requests.get(url=location_endpoint, params=query, headers=headers)
-        code = response.json()["locations"][0]["code"] # refer to example output to understand this line
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
 
-    def get_flight_data(self, origin_city_code, destination_city_code, from_time, to_time):
-        # Uses Tequila Search API Get to retrieve flight data for specified flights
-        # https://tequila.kiwi.com/portal/docs/tequila_api/search_api
-        search_endpoint = f"{TEQUILA_ENDPOINT}/v2/search"
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        location_endpoint = f"{TEQUILA_ENDPOINT}/v2/search"
+        headers = {"apikey": TEQUILA_API_KEY}
         query = {
             "fly_from": origin_city_code,
             "fly_to": destination_city_code,
@@ -37,15 +31,15 @@ class FlightSearch:
             "max_stopovers": 0,
             "curr": "GBP"
         }
-        headers = {"apikey": TEQUILA_API_KEY}
-        response = requests.get(url=search_endpoint, params=query, headers=headers)
+
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+
         try:
             data = response.json()["data"][0]
-        except IndexError: # If no flight found, print error msg and return None
+        except IndexError:
             print(f"No flights found for {destination_city_code}.")
             return None
 
-        # If no exceptions raised, means data is non-empty. We can then format data as a flight_data object.
         flight_data = FlightData(
             price=data["price"],
             origin_city=data["route"][0]["cityFrom"],

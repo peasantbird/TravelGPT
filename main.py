ef01c9ced7de99fc8ba5f81ed816f4f1fea63577
peasantbird <85996382+peasantbird@users.noreply.github.com>
2023-05-28 17:53:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,57 @@ def search_flight(message):
               f"Return date: {result.return_date}"
     bot.reply_to(message, reply)
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['flight'])
+def origin_handler(message):
+    text = "Origin:"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, destination_handler)
+
+def destination_handler(message):
+    origin = message.text.capitalize()
+    text = "Destination:"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, date_from_handler, origin)
+
+def date_from_handler(message, origin):
+    destination = message.text.capitalize()
+    text = "Date from:"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, date_to_handler, origin, destination)
+
+def date_to_handler(message, origin, destination):
+    date_from = message.text
+    text = "Date to:"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, date_to_handler, origin, destination, date_from)
+
+def fetch_flight(message, origin, destination, date_from):
+    date_to = message.text
+    flight = FlightSearch.get_flight_data(origin, destination, date_from, date_to)
+    flight_message = f'*Flight: {flight.price}*'
+    bot.send_message(message.chat.id, flight_message, parse_mode="Markdown")
+
+openai.api_key = ""
+
+@bot.message_handler(commands=['itinerary'])
+def country_handler(message):
+    text = "Where are you planning to go?"
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, fetch_itinerary)
+
+def fetch_itinerary(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"Can you plan a day-to-day detailed itinerary for a trip to {message.text}",
+        temperature=0.5,
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+    message.reply(response.choices[0].text)
+
 
 bot.polling()
 

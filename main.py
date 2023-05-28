a25d6b5b118eee532e2b7207b092e8e93b298968
J-hta-n <103252521+J-hta-n@users.noreply.github.com>
2023-05-28 14:56:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telebot
 
 # BOT_TOKEN = os.environ.get('BOT_TOKEN')
-BOT_TOKEN = "6108579809:AAGWA1jfIwFOrTHuN9ctOVFtzNEr6f8am64"
+BOT_TOKEN = "YOUR TOKEN"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
